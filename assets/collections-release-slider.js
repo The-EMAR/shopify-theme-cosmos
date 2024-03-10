9e23f5f6d0595a9c94554a2004b61aae946e12fe
The-EMAR <mesut.nedzhib@gmail.com>
2024-03-10 18:29:21 +0200

    add files

--- assets/collections-release-slider.js
+++ assets/collections-release-slider.js
@@ -1,59 +1,3 @@
-// (()=>{
-//   if (customElements.get('collections-release-slider')) {
-//     return 0;
-//   }
-
-//   const collectionSliders = document.querySelectorAll('collections-release-slider');
-
-//   if (collectionSliders.length > 0) {
-//     collectionSliders.forEach(el => {
-//       let splide = new Splide(el, {
-//         autoHeight: true,
-//         pagination: false,
-//         perPage: 1,
-//         gap: 20,
-//         arrows: false,
-//         breakpoints: {
-//           1024: {
-//             perPage: 1,
-//           },
-//           767: {
-//             perPage: 1,
-//           }
-//         }
-//       })
-
-//       splide.on( 'pagination:mounted', function ( data ) {
-//         // You can add your class to the UL element
-//         data.list.classList.add( 'splide__pagination--custom' );
-
-//         let slideTitles = el.querySelectorAll('.splide__list li');
-
-//         // `items` contains all dot items
-//         data.items.forEach( function ( item, index_1 ) {
-//           slideTitles.forEach((in_el, index_2)=>{
-//             if (index_1 === index_2) {
-//               item.button.textContent = in_el.getAttribute('data-title');
-//             }
-//           });
-//         } );
-//       } );
-
-//       let thumbnails = new Splide( '#thumbnail-slider', {
-//         perPage: 3,
-//         rewind    : true,
-//         pagination: false,
-//         arrows: false
-//       } ).mount();
-
-//       splide.sync(thumbnails);
-//       splide.mount();
-//       thumbnails.mount();
-
-//     });
-//   }
-// })();
-
 document.addEventListener('DOMContentLoaded', function () {
   var main = new Splide('collections-release-slider', {
     perPage: 1,
@@ -72,17 +16,25 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   var thumbnails = new Splide('#thumbnail-slider', {
-    perPage: 1,
+    perPage: 3,
     focus: 'center',
     trimSpace: false,
     gap: 20,
     pagination: false,
     arrows: false,
     isNavigation: true,
-    height: 50
+    height: 80
   });
 
   main.sync(thumbnails);
   main.mount();
   thumbnails.mount();
 });
+
+// const loadings = document.querySelectorAll('.loading');
+
+// loadings?.forEach(el=>{
+//   if (el.querySelector('img')?.classList.contains('lazyload')) {
+//     el.classList.remove('loading');
+//   }
+// })
