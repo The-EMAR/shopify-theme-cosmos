c378f21d185abad4494035cb54ed11aeb2b16697
The-EMAR <mesut.nedzhib@gmail.com>
2024-03-29 16:14:44 +0200

    Add new slider to main product

--- assets/new-product-slider.js
+++ assets/new-product-slider.js
@@ -1,35 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var main = new Splide('collections-release-slider', {
+  var main3 = new Splide('new-product-slider', {
     perPage: 1,
     gap: 20,
-    autoHeight: true,
     pagination: false,
     arrows: false,
     breakpoints: {
-      1024: {
-        perPage: 1,
+      2056: {
         destroy: true
       },
       767: {
+        destroy: false,
         perPage: 1,
       }
     }
   });
 
-  var thumbnails = new Splide('#thumbnail-slider', {
-    perPage: 1,
-    focus: 'center',
-    trimSpace: false,
-    gap: 20,
-    pagination: false,
-    arrows: false,
-    isNavigation: true,
-    height: 80
-  });
-
-  main.sync(thumbnails);
-  main.mount();
-  thumbnails.mount();
+  main3.mount();
 });
 
 // const loadings = document.querySelectorAll('.loading');
